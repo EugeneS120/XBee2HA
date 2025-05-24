664a1d060950b5ebc6ada961c233eadb3b9f3375
Eugene Shinderman <es11090@gmail.com>
2025-05-24 15:05:32 +0300

    dbg: xbee_bridge load

--- custom_components/xbee_bridge/__init__.py
+++ custom_components/xbee_bridge/__init__.py
@@ -1,11 +1,13 @@
 import logging
 
 _LOGGER = logging.getLogger(__name__)
+_LOGGER.warning("xbee_bridge __init__.py file loaded")
 
 from .mqtt_client import MQTTClient
 
 def setup(hass, config):
     _LOGGER.info("XBee Bridge initializing...")
+    _LOGGER.warning("xbee_bridge async_setup() called")
     conf = config.get("xbee_bridge", {})
     broker = conf.get("mqtt_broker", "localhost")
     port = conf.get("mqtt_port", 1883)
